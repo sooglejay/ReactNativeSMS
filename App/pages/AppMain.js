d5e043c189ceb6c4674bbb3e5dfebf35e04c15c4
sooglejay <394055524@qq.com>
2016-08-27 19:09:55 +0800

    fix login

--- App/pages/AppMain.js
+++ App/pages/AppMain.js
@@ -1,150 +1,153 @@
-// 'use strict';
-
-// import React from 'react';
-// import TextField from 'react-native-md-textinput';
-
-// import {
-//     Dimensions,
-//     Image,
-//     InteractionManager,
-//     View,
-//     ScrollView,
-//     StyleSheet,
-//     Text,
-// } from 'react-native';
-
-// // class AppMain extends React.Component {
-// //     constructor(props) {
-// //         super(props);
-// //     }
-// //     render() {
-// //         return (
-// //             <View><Text>qwqw</Text></View>
-// //         );
-// //     }
-// // }
-// class AppMain extends React.Component {
-//     render() {
-//         return (
-//             <View>
-
-//                 <TextField
-//                     dense={true}
-//                     label={'请输入用户名 '}
-//                     highlightColor={'#00BCD4'}
-//                     keyboardType={'numeric'}
-//                     />
-//                 <TextField
-//                     dense={true}
-//                     label={'请输入用户名 '}
-//                     highlightColor={'#00BCD4'}
-//                     keyboardType={'numeric'}
-//                     />
-//             </View>
-//         );
-//     }
-// }
-// export default AppMain;
-
-
-/**
- * Sample React Native App
- * that demos the react-native-md-textinput
- */
 'use strict';
-import React, { Component } from "react";
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    ScrollView
-} from "react-native";
-
+import React, { Component, PropTypes } from "react";
+import { View, Text, BackAndroid, TouchableOpacity, Image, StyleSheet, InteractionManager, TextInput, Platform, ToastAndroid, NativeModules } from "react-native";
 import TextField from 'react-native-md-textinput';
+import ShortLineTwo from '../components/ShortLineTwo';
 
-const themeColor = '#ffffff';
+var EncryptionModule = NativeModules.EncryptionModule;
+
+
+var username = '';
+var password = '';
+
 class Login extends Component {
-    constructor(props: Object) {
-        super(props);
-        this.inputs = {
-            userName: '',
-            password: '',
-            verifyCode: '',
-        };
-    }
-    render() {
-        let name = '';
-        return (
-            <ScrollView style={styles.container}>
-                <Text style={styles.welcome}>
-                    学生缴费服务平台
-                </Text>
+  constructor(props) {
+    super(props);
+    this.buttonBackAction = this.buttonBackAction.bind(this);
+    this.buttonRegisterOrLoginAction = this.buttonRegisterOrLoginAction.bind(this);
+    this.buttonChangeState = this.buttonChangeState.bind(this);
+    this.findPwdAction = this.findPwdAction.bind(this);
+    this.thirdPartLoginAction = this.thirdPartLoginAction.bind(this);
+  }
+  //返回
+  buttonBackAction() {
+    const {navigator} = this.props;
+    return NaviGoBack(navigator);
+  }
+  //用户登录/注册
+  buttonRegisterOrLoginAction(position) {}
+  buttonChangeState() {}
+  findPwdAction() {}
+  thirdPartLoginAction(position) {}
 
-                <TextField  style={{
-                    paddingTop: 60
-                }}
-                    label={'请输入用户名'}
-                    ref="userName"
-                    highlightColor={themeColor}
-                    returnKeyType={'next'}
-                    onChangeText={(text) => {
-                    } }
-                    inputStyle={styles.textFieldInputStyle}
-                    value={''}
-                    dense={true}
-                    onSubmitEditing={() => this.refs.password.focus() }
-                    />
-                <TextField
-                    ref="password"
-                    secureTextEntry={true}
-                    label={'请输入密码'}
-                    highlightColor={themeColor}
-                    inputStyle={styles.textFieldInputStyle}
-                    onChangeText={(text) => {
-                    } }
-                    returnKeyType={'next'}
-                    onSubmitEditing={() => this.refs.verifyCode.focus() }
-                    dense={true}
-                    onBlur={() => {
-                    } }
-                    />
-                <TextField
-                    ref="verifyCode"
-                    label={'请输入验证码'}
-                    highlightColor={themeColor}
-                    inputStyle={styles.textFieldInputStyle}
-                    onChangeText={(text) => {
-                    } }
-                    onBlur={() => {
-                    } }
-                    />
-            </ScrollView>
-        );
-    }
+  render() {
+    return (
+      <View style={ { backgroundColor: '#2bb4f7', flex: 1 } }>
+        <Image
+               source={ require('../imgs/icon_university.jpg') }
+               style={ { width: 48, height: 48, marginTop: 48, justifyContent: 'center', alignItems: 'center' } } />
+        <Text style={ { justifyContent: center, marginTop: 60 } }>
+          学生缴费服务平台
+        </Text>
+        <View>
+          <View style={ { flexDirection: 'row', backgroundColor: '#72cdf7', borderRadius: 9, height: 45, alignItems: 'center' } }>
+            <Image
+                   source={ require('../imgs/logre/ic_us_icon.png') }
+                   style={ { width: 17, height: 14, marginLeft: 13 } } />
+            <TextInput
+                       style={ { height: 40, fontSize: 15, textAlign: 'left', textAlignVertical: 'center', flex: 1 } }
+                       placeholder="请输入用户名"
+                       placeholderTextColor="#aaaaaa"
+                       underlineColorAndroid="transparent"
+                       numberOfLines={ 1 }
+                       ref={ 'username' }
+                       multiline={ true }
+                       autoFocus={ true }
+                       onChangeText={ (text) => {
+                                        username = text;
+                                      } } />
+          </View>
+          <ShortLineTwo/>
+          <View style={ { flexDirection: 'row', backgroundColor: '#72cdf7', borderRadius: 9, height: 45, alignItems: 'center' } }>
+            <Image
+                   source={ require('../imgs/logre/ic_pwd_icon.png') }
+                   style={ { width: 17, height: 14, marginLeft: 13 } } />
+            <TextInput
+                       style={ { height: 40, fontSize: 15, textAlign: 'left', textAlignVertical: 'center', flex: 1 } }
+                       placeholder="密码"
+                       placeholderTextColor="#aaaaaa"
+                       underlineColorAndroid="transparent"
+                       numberOfLines={ 1 }
+                       ref={ 'password' }
+                       multiline={ true }
+                       secureTextEntry={ true }
+                       onChangeText={ (text) => {
+                                        password = text;
+                                      } } />
+            <TouchableOpacity
+                              onPress={ () => {
+                                          this.buttonChangeState()
+                                        } }
+                              style={ { width: 45, height: 45, alignItems: 'center', justifyContent: 'center' } }>
+              <Image
+                     source={ require('../imgs/logre/ic_pwd_off.png') }
+                     style={ { width: 17, height: 14, marginLeft: 13 } } />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <TouchableOpacity
+                          onPress={ () => {
+                                      this.buttonRegisterOrLoginAction(0)
+                                    } }
+                          style={ { justifyContent: 'center', marginTop: 40, alignItems: 'center' } }>
+          <View style={ { backgroundColor: 'white', borderRadius: 15, width: 300, height: 40, justifyContent: 'center', alignItems: 'center' } }>
+            <Text style={ { color: '#1aa2ed' } }>
+              登录
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <View style={ { alignItems: 'flex-end', marginTop: 13 } }>
+          <TouchableOpacity
+                            onPress={ () => {
+                                        this.findPwdAction()
+                                      } }
+                            style={ { marginRight: 10 } }>
+            <Text style={ { fontSize: 13, color: '#777' } }>
+              找回密码
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      );
+  }
 }
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#2bb5f6',
-        paddingTop: 60,
-        paddingLeft: 16,
-        paddingRight: 16
-    },
-    welcome: {
-        fontSize: 18,
-        textAlign: 'center',
-        margin: 10,
-        color: '#ffffff'
-    },
-    textFieldInputStyle: {
-        paddingTop: 6,
-        color: '#ffffff',
-        fontSize: 18,
-
-    }
-
+  item_layout: {
+    backgroundColor: 'white',
+    height: 48,
+    justifyContent: 'center'
+  },
+  topbar_bg: {
+    height: 48,
+    backgroundColor: 'black',
+    flexDirection: 'row'
+  },
+  topbar_left_item: {
+    width: 48,
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  topbar_center_bg: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  topbar_center_tv: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  topbar_right_item: {
+    width: 48,
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  topbar_right_tv: {
+    fontSize: 15,
+    color: 'white',
+    alignSelf: 'center'
+  }
 });
-
-
 export default Login;
